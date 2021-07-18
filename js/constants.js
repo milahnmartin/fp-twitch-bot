c17f1d9ace388f5abeec2daea7c8ef866bfc8f61
milahnmartin <milahnmartin.develop@gmail.com>
2021-07-18 15:32:20 +0200

    some updates for twitch-bot

--- js/constants.js
+++ js/constants.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.BLOCKED_WORDS = exports.BOT_USERNAME = exports.OAUTH_TOKEN = exports.CHANNEL_NAME = void 0;
 exports.CHANNEL_NAME = 'ultrafy';
 exports.OAUTH_TOKEN = 'pyfxuat0yqzixwe49t0m2q9coqwrlq';
-exports.BOT_USERNAME = 'ultrafy';
+exports.BOT_USERNAME = 'fingerprintza';
 exports.BLOCKED_WORDS = [
     '',
 ];
