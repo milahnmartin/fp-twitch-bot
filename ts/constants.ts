cd99bc71abd8eda1ca1c2318453a1026b9bac324
#@Benjie2302 <milahnmartin.develop@gmail.com>
2021-10-03 17:08:22 +0200

    removed constants

--- ts/constants.ts
+++ ts/constants.ts
@@ -1,23 +1,19 @@
-export const CHANNEL_NAME:string = 'fingerprintza';
-export const OAUTH_TOKEN:string = '818jh3g49itzaod3ys01xqoafu50yd';
+export const CHANNEL_NAME: string = "";
+export const OAUTH_TOKEN: string = "";
 //ultrafy 'pyfxuat0yqzixwe49t0m2q9coqwrlq'
 
-export const BOT_USERNAME:string = 'fingerprintza';
-export const BLOCKED_WORDS:string[] = [
-  'test',
-];
-export const CHANNELS:string[] = ['ultrafy'];
+export const BOT_USERNAME: string = "";
+export const BLOCKED_WORDS: string[] = ["test"];
+export const CHANNELS: string[] = ["ultrafy"];
 
 //MYSQL
 
-export const MYSQL_HOST:string ="fp-logs-database.cqehbngnhfuo.eu-west-1.rds.amazonaws.com";
-export const MYSQL_USER: string="admin";
-export const MYSQL_PASSWORD:string = "#fp-logs2302";
-export const MYSQL_DB:string = "fp-logs";
-
+export const MYSQL_HOST: string = "";
+export const MYSQL_USER: string = "";
+export const MYSQL_PASSWORD: string = "";
+export const MYSQL_DB: string = "";
 
 //discord
 
-
-export const WEBHOOK_TOKEN:string = '3WK4zKkHCIyJK552rDyog7_qpCpYkKFeqNgv-EYv-DvKZd5kb6FRHjSRT4Y8nVe2jw3-'
-export const WEBHOOK_ID:string = '870073204188803072';
+export const WEBHOOK_TOKEN: string = "";
+export const WEBHOOK_ID: string = "";
