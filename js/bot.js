93a786e671c6e515400626dfdd9f34fc66971bfc
#@Benjie2302 <milahnmartin.develop@gmail.com>
2021-10-03 17:10:36 +0200

    add

--- js/bot.js
+++ js/bot.js
@@ -79,8 +79,8 @@ client.on('message', (channel, userstate, message, self) => {
     }
     let command = user_response[0];
     if (command[0] === '!') {
-        logs_1.create_twitch_log(userstate.username, user_response[0], user_response[1]);
-        logs_1.create_twitch_log_discord(userstate.username, user_response[0], user_response[1]);
+        (0, logs_1.create_twitch_log)(userstate.username, user_response[0], user_response[1]);
+        (0, logs_1.create_twitch_log_discord)(userstate.username, user_response[0], user_response[1]);
     }
     ;
     if (user_response[0].toLowerCase() === '!faceit') {
--- js/constants.js
+++ js/constants.js
@@ -1,19 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WEBHOOK_ID = exports.WEBHOOK_TOKEN = exports.MYSQL_DB = exports.MYSQL_PASSWORD = exports.MYSQL_USER = exports.MYSQL_HOST = exports.CHANNELS = exports.BLOCKED_WORDS = exports.BOT_USERNAME = exports.OAUTH_TOKEN = exports.CHANNEL_NAME = void 0;
-exports.CHANNEL_NAME = 'fingerprintza';
-exports.OAUTH_TOKEN = '818jh3g49itzaod3ys01xqoafu50yd';
+exports.CHANNEL_NAME = "";
+exports.OAUTH_TOKEN = "";
 //ultrafy 'pyfxuat0yqzixwe49t0m2q9coqwrlq'
-exports.BOT_USERNAME = 'fingerprintza';
-exports.BLOCKED_WORDS = [
-    'test',
-];
-exports.CHANNELS = ['ultrafy','lxrd_merlin'];
+exports.BOT_USERNAME = "";
+exports.BLOCKED_WORDS = ["test"];
+exports.CHANNELS = ["ultrafy"];
 //MYSQL
-exports.MYSQL_HOST = "fp-logs-database.cqehbngnhfuo.eu-west-1.rds.amazonaws.com";
-exports.MYSQL_USER = "admin";
-exports.MYSQL_PASSWORD = "#fp-logs2302";
-exports.MYSQL_DB = "fp-logs";
+exports.MYSQL_HOST = "";
+exports.MYSQL_USER = "";
+exports.MYSQL_PASSWORD = "";
+exports.MYSQL_DB = "";
 //discord
-exports.WEBHOOK_TOKEN = '3WK4zKkHCIyJK552rDyog7_qpCpYkKFeqNgv-EYv-DvKZd5kb6FRHjSRT4Y8nVe2jw3-';
-exports.WEBHOOK_ID = '870073204188803072';
+exports.WEBHOOK_TOKEN = "";
+exports.WEBHOOK_ID = "";
